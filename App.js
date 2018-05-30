596ce6425544f683fa2a21f6113e7a4c87935446
viktoriyamyalo <viktoriya.myalo.lystopad@gmail.com>
2018-05-30 09:37:28 +0300

    Created reusable boilerplate button

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Button from './Button';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Button/>
+    );
+  }
+}
